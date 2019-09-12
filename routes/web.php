fe9f7b1310f96317caf4620738362154a9fcd8bd
bagusnusantara <bagus.nusantara313@gmail.com>
2019-09-12 22:59:18 +0700

    ubah nama

--- routes/web.php
+++ routes/web.php
@@ -18,62 +18,62 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'PublicController@showhome')->name('PublicHome');
 Route::get('lowongan', 'PublicController@showLowonganpublic')->name('PublicLowongan');
 Route::get('lowongan/show/{id}', 'PublicController@getLowonganpublic')->name('PublicLowonganById');
-Route::post('lamaran/subscribe','JobSeekerController@subscribeLamaran')->name('PublicLowonganSubscribe');
+Route::post('lamaran/subscribe','JobseekerController@subscribeLamaran')->name('PublicLowonganSubscribe');
 
 //-> Jobseeker
 Route::prefix('jobseeker')->group(function(){
     //-x regular
-    Route::get('profil/', 'JobSeekerController@getProfil');
-    Route::get('profil/edit/{id}', 'JobSeekerController@editProfil');
-    Route::get('notifikasi', 'JobSeekerController@getNotifikasi');
-    Route::get('riwayattes', 'JobSeekerController@getRiwayattes');
-    Route::get('lowonganpekerjaan', 'JobSeekerController@getLowonganpekerjaan');
+    Route::get('profil/', 'JobseekerController@getProfil');
+    Route::get('profil/edit/{id}', 'JobseekerController@editProfil');
+    Route::get('notifikasi', 'JobseekerController@getNotifikasi');
+    Route::get('riwayattes', 'JobseekerController@getRiwayattes');
+    Route::get('lowonganpekerjaan', 'JobseekerController@getLowonganpekerjaan');
 
     //-x data diri
-    Route::get('datadiri/', 'JobSeekerController@showDataDiri')->name('JobseekerDatadiri');
-    Route::get('datadiri/insert', 'JobSeekerController@insertDataDiri');
+    Route::get('datadiri/', 'JobseekerController@showDataDiri')->name('JobseekerDatadiri');
+    Route::get('datadiri/insert', 'JobseekerController@insertDataDiri');
 
     //-x Hubungan jobseeker dengan lowongan
-    Route::get('dashboard', 'JobSeekerController@getDashboard')->name('JobseekerDashboard');
-    Route::get('dashboard/show/{id}', 'JobSeekerController@showLowongan');
-    Route::post('lamaran/subscribe','JobSeekerController@subscribeLamaran')->name('JobseekerLowonganSubscribe');
+    Route::get('dashboard', 'JobseekerController@getDashboard')->name('JobseekerDashboard');
+    Route::get('dashboard/show/{id}', 'JobseekerController@showLowongan');
+    Route::post('lamaran/subscribe','JobseekerController@subscribeLamaran')->name('JobseekerLowonganSubscribe');
 
     //-x create method
-    Route::get('profil/riwayatpekerjaan/create', 'JobSeekerController@createRiwayatpekerjaan');
-    Route::get('profil/riwayatpendidikan/create', 'JobSeekerController@createRiwayatPendidikan');
-    Route::get('profil/sertifikatkeahlian/create', 'JobSeekerController@createSertifikatkeahlian');
-    Route::get('profil/kemampuanbahasa/create', 'JobSeekerController@createKemampuanbahasa');
-    Route::get('profil/curiculumvitae/create', 'JobSeekerController@createCuriculumvitae');
-    Route::get('profil/tentangsaya/create', 'JobSeekerController@createTentangsaya');
-    Route::get('notifikasi/create', 'JobSeekerController@createNotifikasi');
-    Route::get('riwayattes/create', 'JobSeekerController@createRiwayattes');
-    Route::get('lowonganpekerjaan/create', 'JobSeekerController@createtLowonganpekerjaan');
+    Route::get('profil/riwayatpekerjaan/create', 'JobseekerController@createRiwayatpekerjaan');
+    Route::get('profil/riwayatpendidikan/create', 'JobseekerController@createRiwayatPendidikan');
+    Route::get('profil/sertifikatkeahlian/create', 'JobseekerController@createSertifikatkeahlian');
+    Route::get('profil/kemampuanbahasa/create', 'JobseekerController@createKemampuanbahasa');
+    Route::get('profil/curiculumvitae/create', 'JobseekerController@createCuriculumvitae');
+    Route::get('profil/tentangsaya/create', 'JobseekerController@createTentangsaya');
+    Route::get('notifikasi/create', 'JobseekerController@createNotifikasi');
+    Route::get('riwayattes/create', 'JobseekerController@createRiwayattes');
+    Route::get('lowonganpekerjaan/create', 'JobseekerController@createtLowonganpekerjaan');
 
     //-x Store method
-    Route::post('datadiri/submitdatadiriawal/', 'JobSeekerController@storeDataDiriAwal');
-    Route::post('datadiri/submitdatadiri/', 'JobSeekerController@storeDataDiri');
-    Route::post('datadiri/submitpendidikanformal/', 'JobSeekerController@storeDataPendidikanFormal');
-    Route::post('datadiri/submitpendidikaninformal/', 'JobSeekerController@storeDataPendidikanInformal');
-    Route::post('datadiri/submitpendidikanbahasa/', 'JobSeekerController@storeDataPendidikanBahasa');
-    Route::post('datadiri/submitpengalamankerja/', 'JobSeekerController@storeDataPengalamanKerja');
-    Route::post('datadiri/submitpengalamanorganisasi/', 'JobSeekerController@storeDataPengalamanOrganisasi');
-    Route::post('datadiri/submitriwayatpenyakit/', 'JobSeekerController@storeDataRiwayatPenyakit');
-    Route::post('datadiri/submitminat/', 'JobSeekerController@storeDataMinat');
-    Route::post('datadiri/submitlampiran/', 'JobSeekerController@storeDataLampiran');
+    Route::post('datadiri/submitdatadiriawal/', 'JobseekerController@storeDataDiriAwal');
+    Route::post('datadiri/submitdatadiri/', 'JobseekerController@storeDataDiri');
+    Route::post('datadiri/submitpendidikanformal/', 'JobseekerController@storeDataPendidikanFormal');
+    Route::post('datadiri/submitpendidikaninformal/', 'JobseekerController@storeDataPendidikanInformal');
+    Route::post('datadiri/submitpendidikanbahasa/', 'JobseekerController@storeDataPendidikanBahasa');
+    Route::post('datadiri/submitpengalamankerja/', 'JobseekerController@storeDataPengalamanKerja');
+    Route::post('datadiri/submitpengalamanorganisasi/', 'JobseekerController@storeDataPengalamanOrganisasi');
+    Route::post('datadiri/submitriwayatpenyakit/', 'JobseekerController@storeDataRiwayatPenyakit');
+    Route::post('datadiri/submitminat/', 'JobseekerController@storeDataMinat');
+    Route::post('datadiri/submitlampiran/', 'JobseekerController@storeDataLampiran');
 
     //delete data
-    Route::delete('datadiri/deletependidikanformal/', 'JobSeekerController@destroyDataPendidikanFormal');
-    Route::delete('datadiri/deletependidikaninformal/', 'JobSeekerController@destroyDataPendidikanInformal');
-    Route::delete('datadiri/deletependidikanbahasa/', 'JobSeekerController@destroyDataPendidikanBahasa');
-    Route::delete('datadiri/deletepengalamankerja/', 'JobSeekerController@destroyDataPengalamanKerja');
-    Route::delete('datadiri/deletepengalamanorganisasi/', 'JobSeekerController@destroyDataPengalamanOrganisasi');
-    Route::delete('datadiri/deleteriwayatpenyakit/', 'JobSeekerController@destroyDataRiwayatPenyakit');
-    Route::delete('datadiri/deleteminat/', 'JobSeekerController@destroyDataMinat');
-    Route::delete('datadiri/deletelampiran/', 'JobSeekerController@destroyDataLampiran');
+    Route::delete('datadiri/deletependidikanformal/', 'JobseekerController@destroyDataPendidikanFormal');
+    Route::delete('datadiri/deletependidikaninformal/', 'JobseekerController@destroyDataPendidikanInformal');
+    Route::delete('datadiri/deletependidikanbahasa/', 'JobseekerController@destroyDataPendidikanBahasa');
+    Route::delete('datadiri/deletepengalamankerja/', 'JobseekerController@destroyDataPengalamanKerja');
+    Route::delete('datadiri/deletepengalamanorganisasi/', 'JobseekerController@destroyDataPengalamanOrganisasi');
+    Route::delete('datadiri/deleteriwayatpenyakit/', 'JobseekerController@destroyDataRiwayatPenyakit');
+    Route::delete('datadiri/deleteminat/', 'JobseekerController@destroyDataMinat');
+    Route::delete('datadiri/deletelampiran/', 'JobseekerController@destroyDataLampiran');
 
     //-x support ajax
     Route::post('support/getst/', 'SupportController@getSt');
 
 
-    Route::get('lampiran/{kategori}','JobSeekerController@getLampiran');
+    Route::get('lampiran/{kategori}','JobseekerController@getLampiran');
 });
