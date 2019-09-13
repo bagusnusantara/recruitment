c1f94f7fccd619276d5310fdab74b626062d1223
bagusnusantara <bagus.nusantara313@gmail.com>
2019-09-13 10:05:19 +0700

    pdf

--- resources/views/admin/sdm/index.blade.php
+++ resources/views/admin/sdm/index.blade.php
@@ -24,7 +24,7 @@
             <div class="col-lg-12">
                 <div class="ibox float-e-margins">
                     <div class="ibox-title">
-                        <h5>List SDM</h5>
+                        <h5>List Data Pelamar</h5>
 
                         <div class="ibox-tools">
                             <a class="collapse-link">
